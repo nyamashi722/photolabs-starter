aedc8b70d59846b2472ad6188ee6e6bea0ee56a7
Nathan Yamashiro <n.yamashiro722@gmail.com>
2023-11-06 20:22:40 -0500

    add onclick handler to toggle fav when user clicks fav button

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,13 +1,15 @@
-import React, { useCallback, useState } from 'react';
+import React from 'react';
 
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
-function PhotoFavButton() {
+function PhotoFavButton({photoId, favourite, toggleFav}) {
+  const selected = favourite.includes(photoId)
+
   return (
-    <div className="photo-list__fav-icon">
+    <div onClick={() => {toggleFav(photoId)}} className="photo-list__fav-icon">
       <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+        <FavIcon selected={selected}/>
       </div>
     </div>
   );
