fbb16bb9c84e85571b71b977bb74b26e19c1570e
Nathan Yamashiro <n.yamashiro722@gmail.com>
2023-11-06 20:18:25 -0500

    check if there are any fav photos and display notif if there are

--- frontend/src/components/FavBadge.jsx
+++ frontend/src/components/FavBadge.jsx
@@ -3,7 +3,9 @@ import FavIcon from './FavIcon';
 
 import '../styles/FavBadge.scss';
 
-const FavBadge = ({ isFavPhotoExist }) => {
+const FavBadge = ({favourite}) => {
+  const isFavPhotoExist = favourite.length >= 1
+
   return (
     <div className='fav-badge'>
       <FavIcon displayAlert={!!isFavPhotoExist}/>
