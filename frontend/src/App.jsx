3660cfb85d409d65d9e4869db20c2d0127007111
Nathan Yamashiro <n.yamashiro722@gmail.com>
2023-11-06 20:17:09 -0500

    set up routes to Homeroute and modal components and set up global state for modal and fav photos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
-
-import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
+import HomeRoute from 'routes/HomeRoute';
+import topics from 'mocks/topics';
+import photos from 'mocks/photos';
+import { useState } from 'react';
+import PhotoDetailsModal from 'routes/PhotoDetailsModal';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
+  
+  const [favourite, setFavourite] = useState([]);
+  const toggleFav = (photoId) => {
+    const photoIsInFav = favourite.includes(photoId)
+    !!photoIsInFav ? setFavourite([...favourite.filter((id) => id !== photoId)]) : setFavourite([...favourite, photoId]);
+  }
+
+  const [modal, setModal] = useState(null);
+  const showModal = (photoData) => setModal(photoData);
+  const closeModal = () => setModal(null);
+
   return (
     <div className="App">
-      <PhotoListItem/>
+      <HomeRoute topics={topics} photos={photos} favourite={favourite} toggleFav={toggleFav} showModal={showModal}/>
+      {!!modal && <PhotoDetailsModal modal={modal} closeModal={closeModal} favourite={favourite} toggleFav={toggleFav} photos={photos}/>}
     </div>
   );
 };
