119d53d3a4f986a94039f4a34f7c03317c391372
Nathan Yamashiro <n.yamashiro722@gmail.com>
2023-11-06 20:32:54 -0500

    make home route parent component where nav bar and photolist components are called

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-
+import TopNavigation from 'components/TopNavigationBar';
+import PhotoList from 'components/PhotoList';
 import '../styles/HomeRoute.scss';
 
-const HomeRoute = () => {
+const HomeRoute = ({topics, photos, favourite, toggleFav, showModal}) => {
+
   return (
     <div className="home-route">
-      {/* Insert React */}
+      <TopNavigation topics={topics} favourite={favourite}/>
+      <PhotoList photos={photos} favourite={favourite} toggleFav={toggleFav} showModal={showModal}/> 
     </div>
   );
 };
