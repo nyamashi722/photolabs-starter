696042ece0ea11342a30e26b8c3db8185a937727
Nathan Yamashiro <n.yamashiro722@gmail.com>
2023-11-06 20:31:49 -0500

    make array of topic list items

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,29 +1,20 @@
 import React from "react";
-
+import TopicListItem from "./TopicListItem";
 import "../styles/TopicList.scss";
 
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
+const TopicList = ({ topics }) => {
+  const topicArray = topics.map((topic) => {
+    return (
+      <TopicListItem
+        {...topic}
+        key={topic.id}
+      />
+    )
+  })
 
-const TopicList = () => {
   return (
     <div className="top-nav-bar__topic-list">
-      {/* Insert React */}
+      {topicArray}
     </div>
   );
 };
