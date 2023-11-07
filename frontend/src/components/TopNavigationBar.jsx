de92b9ab4f16737301bc631681757a4794518118
Nathan Yamashiro <n.yamashiro722@gmail.com>
2023-11-06 20:31:22 -0500

    make nav bar component with mock data

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import TopicList from './TopicList';
+import FavBadge from './FavBadge';
 
 import '../styles/TopNavigationBar.scss'
 
-const TopNavigation = () => {
+const TopNavigation = ({topics, favourite}) => {
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
+      <TopicList topics={topics}/>
+      <FavBadge favourite={favourite}/>
     </div>
   )
 }
