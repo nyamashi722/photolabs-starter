af662f94c11179b0b0fae0a45a0e4f32b6e678ee
Nathan Yamashiro <n.yamashiro722@gmail.com>
2023-11-06 20:33:33 -0500

    make modal component with functioning close button, main photo and similar photos

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,14 +1,33 @@
 import React from 'react';
-
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
+import PhotoFavButton from 'components/PhotoFavButton';
+import PhotoList from 'components/PhotoList';
 
-const PhotoDetailsModal = () => {
+const PhotoDetailsModal = ({ modal, closeModal, favourite, toggleFav, photos }) => {
+  const similarPhotos = Object.values(modal.similar_photos);
   return (
     <div className="photo-details-modal">
       <button className="photo-details-modal__close-button">
-        <img src={closeSymbol} alt="close symbol" />
+        <img src={closeSymbol} alt="close symbol" onClick={closeModal} />
       </button>
+      <div className="photo-list__item">
+        <PhotoFavButton photoId={modal.id} favourite={favourite} toggleFav={toggleFav} />
+        <img src={modal.urls.regular} className="photo-details-modal__image" />
+        <div className="photo-details-modal__photographer-details">
+          <img src={modal.user.profile} className="photo-details-modal__photographer-profile" />
+          <div className="photo-details-modal__photographer-info">
+            <p className="photo-list__username">{modal.user.username}</p>
+            <p className="photo-details-modal__photographer-location">
+              {modal.location.city}, {modal.location.country}
+            </p>
+          </div>
+        </div>
+      </div>
+      <header className='photo-details-modal__header'>Similar Photos</header>
+      <div className='photo-details-modal__images'>
+        <PhotoList similarPhotos={similarPhotos} photos={photos} favourite={favourite} toggleFav={toggleFav} modal={modal} />
+      </div>
     </div>
   )
 };
